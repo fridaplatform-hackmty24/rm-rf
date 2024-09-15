3a4b5430210301f0e683ccc77df61c8223e18321
Leonardo cervantes <leocerva29@gmail.com>
2024-09-15 11:35:32 -0600

    added files from cloned repo

---
+++ app/(components)/TicketCard.jsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import DeleteBlock from "./DeleteBlock";
+import PriorityDisplay from "./PriorityDisplay";
+import ProgressBar from "./ProgressBar";
+import StatusDisplay from "./StatusDisplay";
+
+// Actualizamos el componente para aceptar props
+const TicketCard = ({ id, valor, sector, estatus }) => {
+  const router = useRouter(); // Initialize the router
+
+  const handleCardClick = () => {
+    router.push(`/TicketPage/${id}`); // Navigate to the respective ticket page
+  };
+
+  return (
+    <div
+      onClick={handleCardClick}
+      className="flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2 cursor-pointer"
+    >
+      <div className="flex mb-3">
+        <PriorityDisplay priority={valor} />{" "}
+        {/* Usamos valor para el componente Priority */}
+        <div className="ml-auto">
+          <DeleteBlock />
+        </div>
+      </div>
+      <h4>Ticket ID: {id}</h4> {/* Mostramos el ID del ticket */}
+      <hr className="h-px border-0 bg-page mb-2" />
+      <p className="whitespace-pre-wrap">
+        Sector de Fuga: {sector} {/* Mostramos el sector */}
+      </p>
+      <p className="whitespace-pre-wrap">
+        Estatus: {estatus} {/* Mostramos el estatus */}
+      </p>
+      <div className="flex-grow"></div>
+      <div className="flex mt-2">
+        <div className="flex flex-col">
+          <p className="text-xs my-1">08/31/23 10:40PM</p>
+          <ProgressBar estatus={estatus} />
+        </div>
+        <div className="ml-auto flex items-end">
+          <StatusDisplay estatus={estatus} />{" "}
+          {/* Usamos estatus para el StatusDisplay */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TicketCard;
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        nav: "#55AD9B",
+        page: "#F1F8E8",
+        card: "#bafbda",
+        "card-hover": "#D8EFD3",
+        "default-text": "#17153B",
+        "blue-accent": "#55AD9B",
+        "blue-accent-hover": "#D8EFD3",
+      },
+    },
+  },
+  plugins: [],
+};
